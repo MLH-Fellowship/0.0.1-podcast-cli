be44fa403eba84e755fed5cedb6445d0e8d3f9d1
William Ma <whoiswillma@gmail.com>
2020-06-03 15:55:02 -0400

    Add help message, sanity checking

--- podcast.plugin.zsh
+++ podcast.plugin.zsh
@@ -34,16 +34,64 @@ function podcast() {
     local opt=$1
     case "$opt" in
         search)
+            if [[ $2 == '' ]] 
+            then 
+                echo "podcast search: please enter a search term"
+                return 1
+            fi
+
+            _podcast_search $2
             ;; 
         imfeelinglucky)
+            _podcast_random
             ;;
         download) 
+            if [[ $2 == '' ]]
+            then
+                echo "podcast download: please enter the short url of a dev.to podcast"
+                return 1
+            fi
+
+            if [[ $3 == '' ]]
+            then 
+                _podcast_download $2 $3
+            else
+                _podcast_download $2 './podcast'
+            fi
             ;;
         info)
+            if [[ $2 == '' ]] 
+            then 
+                echo "podcast info: please enter the short url of a dev.to podcast"
+                return 1
+            fi
+
+            _podcast_info $2
             ;;
         list)
+            if [[ $2 == '' ]]
+            then 
+                echo "podcast list: please enter the page number"
+                return 1
+            fi
+
+            if [[ $3 == '' ]]
+            then 
+                echo "podcast list: please enter the number of search results"
+                return 1
+            fi
+
+            _podcast_list $2 $3
             ;;
         ""|-h|--help)
+            echo "Usage: podcast <option>"
+            echo "-h, --help\n\tShow help information"
+            echo "search <search_term>\n\tSearch recent podcasts for the search term"
+            echo "imfeelinglucky\n\tShow a random podcast episode"
+            echo "podcast download <short_url> <?download_dir>\n\tDownload a podcast"
+            echo "\tIf the download directory is missing, this will download to ./podcasts"
+            echo "info <short_url>\n\tDisplay the show art and any metadata"
+            echo "list <pg_num> <pg_size>\n\tBrowse the list of podcasts page by page"
             ;;
         *)
             echo "Unknown option: $opt"
